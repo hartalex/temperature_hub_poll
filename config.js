48fa1b058abac6c0787424c81a371e5737d23db0
Alex Hart <alex@hartcode.com>
2017-08-31 20:40:23 +0000

    Update url

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // this is a temp config file
 // replace with your own configuration
 
-module.exports = { hubUrl: 'http://test.hartcode.com/' }
+module.exports = { hubUrl: 'http://hub.hartcode.com/' }
